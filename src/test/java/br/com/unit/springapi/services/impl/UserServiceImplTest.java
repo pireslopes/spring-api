803319aded321f7d4d7f0a1088a7e4e9231b39ee
pireslopes <pireselis@hotmail.com>
2023-03-29 00:14:05 -0300

    add final tests userServiceImpl

--- src/test/java/br/com/unit/springapi/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/unit/springapi/services/impl/UserServiceImplTest.java
@@ -3,8 +3,8 @@ package br.com.unit.springapi.services.impl;
 import br.com.unit.springapi.domain.User;
 import br.com.unit.springapi.domain.dto.UserDto;
 import br.com.unit.springapi.repositories.UserRepository;
+import br.com.unit.springapi.services.exceptions.DataIntegratyViolationException;
 import br.com.unit.springapi.services.exceptions.ObjectNotFoundException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -14,10 +14,11 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -72,18 +73,79 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() {
+        var current = List.of(getUser());
+        Mockito.when(repository.findAll()).thenReturn(current);
+
+        var expected = service.findAll();
+
+        assertTrue(expected.size() > 0);
+
+        assertIterableEquals(expected, current);
     }
 
     @Test
-    void add() {
+    void whenAddWithCorrectInputThenReturnSuccess() {
+        Mockito.when(repository.save(Mockito.any())).thenReturn(user);
+
+        var response = service.add(dto);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
     }
 
     @Test
-    void update() {
+    void whenAddWithExistUserThenReturnDataIntegratyViolationException() {
+        Mockito.when(repository.findByEmail(Mockito.any())).thenReturn(optional);
+
+        try {
+            dto.setId(3);
+            service.add(dto);
+        } catch (Exception ex) {
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals("E-mail already registered!", ex.getMessage());
+        }
     }
 
     @Test
-    void delete() {
+    void whenUpdateWithCorrectInputThenReturnSuccess() {
+        Mockito.when(repository.save(Mockito.any())).thenReturn(user);
+
+        var response = service.update(dto);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+    }
+
+    @Test
+    void whenUpdateWithExistUserThenReturnDataIntegratyViolationException() {
+        Mockito.when(repository.findByEmail(Mockito.any())).thenReturn(optional);
+
+        try {
+            dto.setId(3);
+            service.update(dto);
+        } catch (Exception ex) {
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals("E-mail already registered!", ex.getMessage());
+        }
+    }
+
+    @Test
+    void whenDeleteWithExistsUserThenExecuteSuccess() {
+        Mockito.when(repository.findById(Mockito.anyInt())).thenReturn(optional);
+        Mockito.doNothing().when(repository).deleteById(Mockito.anyInt());
+        service.delete(getUser().getId());
+        Mockito.verify(repository, Mockito.times(1)).deleteById(Mockito.anyInt());
+    }
+
+    @Test
+    void whenDeleteWithNotExistsUserThenThrowObjectNotFoundException() {
+        Mockito.when(repository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
+        try {
+            service.delete(getUser().getId() + 9);
+        } catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals("Object Not Found!", ex.getMessage());
+        }
     }
 }
